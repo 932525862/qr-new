3ef211d7ab54c3a72819a6c5b2bd9136aa249385
muhammadyusuf <muhammadyusuf@kali.muhammadyusuf>
2025-01-21 11:43:41 +0500

    manba

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { QRCodeCanvas } from "qrcode.react";
-function App() {
 
+function App() {
   const url = "https://preparati-sertifikat.vercel.app/";
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1></h1>
+      <h1>QR Kod Sinovi</h1>
       <p>Quyidagi QR kodni skaner qilib sinab ko'ring:</p>
       <QRCodeCanvas
         value={url}
-        size={150} // Hajmi siz bergan rasmdagi hajmga moslashadi
-        bgColor={"#ffffff"} // Orqa fon rangini oq qilamiz
-        fgColor={"#000000"} // QR kod elementlarini qora rangda sozlaymiz
-        level={"L"} // Kamroq ma'lumot bo'lsa, to'g'ri aniqlash
+        size={180} // Rasm hajmiga mos kattalik
+        bgColor={"#ffffff"} // Orqa fon oq rangda
+        fgColor={"#000000"} // QR kod qora rangda
+        level={"L"} // Past darajadagi aniqlik
         includeMargin={true} // Bo'sh joyni qo'shish
       />
     </div>
